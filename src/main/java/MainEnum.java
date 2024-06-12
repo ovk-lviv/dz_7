b2e2202a30c2f6bb833185dcc0075718f98a9768
ovk-lviv <ovk.16.07.2019@gmail.com>
2024-06-12 21:04:37 +0300

    first commit

---
+++ src/main/java/MainEnum.java
@@ -0,0 +1,9 @@
+public class MainEnum {
+
+    public static void main(String[] args) {
+
+        SolarSystem m1 = SolarSystem.NEPTUNE;
+
+        System.out.println(SolarSystem.getNext(SolarSystem.NEPTUNE));
+    }
+}
